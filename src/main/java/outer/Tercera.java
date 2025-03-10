55c9e269aa4eb51f72523c039f551160ae3b3280
arnaumeseguer <arnaumeseguer@iesebre.com>
2025-03-10 18:21:09 +0100

    'Primer commit'

--- src/main/java/outer/Tercera.java
+++ src/main/java/outer/Tercera.java
@@ -7,7 +7,7 @@ import org.example.AlumnePublic;
 public class Tercera {
     public static void main(String []args){
         AlumnePublic a=new AlumnePublic();
-        //dni és protectes i només podem accedir desde classes del mateix paquet on es troba AlumnePublic
+        //dni és protected i només podem accedir desde classes del mateix paquet on es troba AlumnePublic
         //System.out.println(a.dni);
 
     }
