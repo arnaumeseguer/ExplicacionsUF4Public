7c18e3546e34d884faea8218d6f1d29c22c46b7d
arnaumeseguer <arnaumeseguer@iesebre.com>
2025-03-10 13:57:34 +0100

    Primer commit

---
+++ src/main/java/org/example/Segona.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Segona {
+    public static void main(String[] args) {
+        Alumne a=new Alumne("Joan");;
+        System.out.println("Nom: " + a.dni);
+    }
+}
---
+++ src/main/java/outer/Tercera.java
@@ -0,0 +1,14 @@
+package outer;
+
+import org.example.AlumnePublic;
+
+import org.example.AlumnePublic;
+
+public class Tercera {
+    public static void main(String []args){
+        AlumnePublic a=new AlumnePublic();
+        //dni és protectes i només podem accedir desde classes del mateix paquet on es troba AlumnePublic
+        //System.out.println(a.dni);
+
+    }
+}
