cc723c4309b328ea4f039838b0494d89fcb0b1e3
pabloeldelhub <pablomaso@iesebre.com>
2025-03-18 09:14:14 +0100

    Delete src/main/java directory

--- src/main/java/org/example/AlumnePublic.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class AlumnePublic {
-    //Propietats
-    private String nom = "Josep";
-    protected String dni; // Es veu sempre i quan estiguis al mateix paquet
-
-}
-
